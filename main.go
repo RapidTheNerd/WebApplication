e01213832b56b01d1ec018f4dc35698509a4f30b
RapidTheNerd <george.marr.cobc@gmail.com>
2017-04-14 00:58:54 +0100

    holy shit this is way different than C#

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+)
 
 func main(){
 	http.HandleFunc("/", responseWriter)
@@ -9,4 +13,29 @@ func main(){
 
 func responseWriter(w http.ResponseWriter, req * http.Request){
 	w.Write([]byte("General test"))
+}
+
+type Page struct {
+	title string
+	body []byte
+}
+
+func(p* Page) save() error {
+	f := p.title + ".txt"
+	return ioutil.WriteFile(f, p.body, 0600)
+}
+
+func load(title string) (*Page, error){
+	f:= title+ ".txt"
+	body, err := ioutil.ReadFile(f)
+	if err != nil{
+		return nil, err
+	}
+	return &Page{title, body}, nil
+
+}
+func view(w http.ResponseWriter, r * http.Request){
+	title := r.URL.Path[len("/test/"):]
+	p, _ := load(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>&s</div>", p.title, p.body)
 }
